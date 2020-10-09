f271da9df288941699348df9b02c17d935a9f13e
vilsku <aspegren.vilja@gmail.com>
2020-10-09 03:28:57 +0300

    test

--- public/chat.js
+++ public/chat.js
@@ -1,16 +1,3 @@
-'use strict';
-/*const socket = io('http://10.114.32.18');
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const inp = document.getElementById('m');
-  socket.emit('chat message', inp.value);
-  inp.value = '';
-});
-socket.on('chat message', (msg) => {
-  const item = document.createElement('li');
-  item.innerHTML = msg;
-  document.getElementById('messages').appendChild(item);
-}); */
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
 const roomName = document.getElementById("room-name");
